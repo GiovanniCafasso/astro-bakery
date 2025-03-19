0050db2749487fe0fc21830c03e6e126ec1fa708
Giovanni Cafasso <g.cafasso@nexionengineering.com>
2025-03-19 18:01:10 +0100

    update astro.config.mjs with site link

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://astro-bakery.pages.dev', // Sostituisci con l'URL del tuo sito su Cloudflare Pages
   vite: {
     plugins: [tailwindcss()]
   }
